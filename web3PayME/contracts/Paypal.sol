69c116484b6f5d9d8ee35a1c2f103e95ec086f44
CodeThyDominus <thydominus@gmail.com>
2023-10-20 01:48:18 +0530

    check association between username and address

--- web3PayME/contracts/Paypal.sol
+++ web3PayME/contracts/Paypal.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Paypal{
+contract Paypal {
 
     address public owner;
 
-    constructor(){
-        owner = msg sender;
+    constructor() {
+        owner = msg.sender; // Corrected msg.sender assignment
     }
 
-    struct request{
+    struct Request { // Corrected struct name to start with an uppercase letter
         address requester;
-        uint256 ampunt;
+        uint256 amount;
         string message;
         string name;
     }
 
-    struct sendReceive{
+    struct SendReceive { // Corrected struct name to start with an uppercase letter
         string action;
         uint256 amount;
         string message;
@@ -24,71 +24,72 @@ contract Paypal{
         string otherPartyName;
     }
 
-    struct userName{
+    struct UserName { // Corrected struct name to start with an uppercase letter
         string name;
+        bool hasName;
     }
 
-    // mapping for request, transaction and userName
-    mapping(address => userName) names;
-    mapping(address => request[]) requests;
-    mapping(address => sendReceive[]) history;
+    // mapping for request, transaction, and userName
+    mapping(address => UserName) public names;
+    mapping(address => Request[]) public requests; // Added "public" to make it accessible
+    mapping(address => SendReceive[]) public history; // Added "public" to make it accessible
 
-    //adding name to wallet
-    function addName(string memory) public{
-        userName storage newUserName = names[msg.sender];
-        newUserName = _name;
+    // Adding name to wallet
+    function addName(string memory _name) public {
+        UserName storage newUserName = names[msg.sender];
+        newUserName.name = _name; // Corrected the assignment of name
+        newUserName.hasName = true;
     }
 
-    //creating request
-    function createRequest(address user, uint256 _amount, string memory _message) public{
-        request memory newRequest;
+    // Creating request
+    function createRequest(address user, uint256 _amount, string memory _message) public {
+        Request memory newRequest;
         newRequest.requester = msg.sender;
         newRequest.amount = _amount;
         newRequest.message = _message;
+
+        // Checking the association between username and address
+        if (names[msg.sender].hasName) {
+            newRequest.name = names[msg.sender].name;
+        }
+
         requests[user].push(newRequest);
     }
 
-    //payment for request
-    function payRequest(uint256 _request) public payable{
-        require(_request < requests[msg.sender].length, "No Such Rewuest Found!");
-        request storage payableRequest = myRequests[_request];
-        
-    uint256 toPay = payableRequest.amount * 1000000000000000000;
-    require(msg.value == (toPay), "Pay Correct Amount");
+    // Payment for request
+    function payRequest(uint256 _request) public payable {
+        require(_request < requests[msg.sender].length, "No Such Request Found!");
+        Request storage payableRequest = requests[msg.sender][_request]; // Corrected variable name
 
-    payable(payableRequest.requester).transfer(msg.value);
+        uint256 toPay = payableRequest.amount * 1 ether; // Corrected the conversion
+        require(msg.value == toPay, "Pay Correct Amount");
 
-    addHistory(msg.sender, payableRequest.requester, payableRequest.amount, payableRequest.message);
+        payable(payableRequest.requester).transfer(msg.value);
 
-    myRequests[_request] = myRequests[myRequests.length-1];
-    myRequests.pop();
+        requests[msg.sender][_request] = requests[msg.sender][requests[msg.sender].length - 1]; // Corrected variable names
+        requests[msg.sender].pop();
     }
-    
-    //sending request to user
+
+    // Sending request to user
     function getMyRequests(address _user) public view returns(
-         address[] memory, 
-         uint256[] memory, 
-         string[] memory, 
-         string[] memory
-){
+        address[] memory, 
+        uint256[] memory, 
+        string[] memory, 
+        string[] memory) {
 
         address[] memory addrs = new address[](requests[_user].length);
         uint256[] memory amnt = new uint256[](requests[_user].length);
         string[] memory msge = new string[](requests[_user].length);
         string[] memory nme = new string[](requests[_user].length);
-        
+
         for (uint i = 0; i < requests[_user].length; i++) {
-            request storage myRequests = requests[_user][i];
-            addrs[i] = myRequests.requestr;
-            amnt[i] = myRequests.amount;
-            msge[i] = myRequests.message;
-            nme[i] = myRequests.name;
+            Request storage myRequest = requests[_user][i]; // Corrected variable name
+            addrs[i] = myRequest.requester;
+            amnt[i] = myRequest.amount;
+            msge[i] = myRequest.message;
+            nme[i] = myRequest.name;
         }
-        
-        return (addrs, amnt, msge, nme);        
-         
 
-}
-    
-
+        return (addrs, amnt, msge, nme);
+    }   
 }
