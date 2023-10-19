78853d6672682e5b2581f88df5ed01602cd9ab20
CodeThyDominus <thydominus@gmail.com>
2023-10-20 01:00:27 +0530

    creating smart contract

--- web3PayME/contracts/Paypal.sol
+++ web3PayME/contracts/Paypal.sol
@@ -1,28 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Paypal {
-    // S1 : Define the owner of the smart contract
+contract Paypal{
 
     address public owner;
 
-    constructor() {
-        owner = msg.sender;
+    constructor(){
+        owner = msg sender;
     }
 
-    // S2 :create struct and mapping for request,transaction and name
-
-    struct request {
-        address requestor;
-        uint256 amount;
+    struct request{
+        address requester;
+        uint256 ampunt;
         string message;
         string name;
     }
 
-    struct sendReceive {
+    struct sendReceive{
         string action;
         uint256 amount;
         string message;
@@ -30,55 +24,71 @@ contract Paypal {
         string otherPartyName;
     }
 
-    struct userName {
+    struct userName{
         string name;
-        bool hasName;
     }
 
+    // mapping for request, transaction and userName
     mapping(address => userName) names;
     mapping(address => request[]) requests;
     mapping(address => sendReceive[]) history;
 
-    //S3 :Add a name to wallet address
-    function addName(string memory _name) public {
+    //adding name to wallet
+    function addName(string memory) public{
         userName storage newUserName = names[msg.sender];
-        newUserName.name = _name;
-        newUserName.hasName = true;
+        newUserName = _name;
     }
 
-    //S4 :create a Request
-
-    function createRequest(
-        address user,
-        uint256 _amount,
-        string memory _message
-    ) public {
+    //creating request
+    function createRequest(address user, uint256 _amount, string memory _message) public{
         request memory newRequest;
-        newRequest.requestor = msg.sender;
+        newRequest.requester = msg.sender;
         newRequest.amount = _amount;
         newRequest.message = _message;
-        if (names[msg.sender].hasName) {
-            newRequest.name = names[msg.sender].name;
-        }
         requests[user].push(newRequest);
     }
 
-    //S5 :Pay a request
-
-  function payRequest(uint256 _request) public payable {
-    
-    require(_request < requests[msg.sender].length, "No Such Request");
-    request[] storage myRequests = requests[msg.sender];
-    request storage payableRequest = myRequests[_request];
+    //payment for request
+    function payRequest(uint256 _request) public payable{
+        require(_request < requests[msg.sender].length, "No Such Rewuest Found!");
+        request storage payableRequest = myRequests[_request];
         
     uint256 toPay = payableRequest.amount * 1000000000000000000;
     require(msg.value == (toPay), "Pay Correct Amount");
 
-    payable(payableRequest.requestor).transfer(msg.value);
+    payable(payableRequest.requester).transfer(msg.value);
 
-    addHistory(msg.sender, payableRequest.requestor, payableRequest.amount, payableRequest.message);
+    addHistory(msg.sender, payableRequest.requester, payableRequest.amount, payableRequest.message);
 
     myRequests[_request] = myRequests[myRequests.length-1];
     myRequests.pop();
+    }
+    
+    //sending request to user
+    function getMyRequests(address _user) public view returns(
+         address[] memory, 
+         uint256[] memory, 
+         string[] memory, 
+         string[] memory
+){
+
+        address[] memory addrs = new address[](requests[_user].length);
+        uint256[] memory amnt = new uint256[](requests[_user].length);
+        string[] memory msge = new string[](requests[_user].length);
+        string[] memory nme = new string[](requests[_user].length);
+        
+        for (uint i = 0; i < requests[_user].length; i++) {
+            request storage myRequests = requests[_user][i];
+            addrs[i] = myRequests.requestr;
+            amnt[i] = myRequests.amount;
+            msge[i] = myRequests.message;
+            nme[i] = myRequests.name;
+        }
+        
+        return (addrs, amnt, msge, nme);        
+         
+
+}
+    
 
 }
